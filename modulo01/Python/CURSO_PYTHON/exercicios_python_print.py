a4263dd9f5d690c41b3465453bdd26384eb1d27f
JVAS42 <jvasprogramando@gmail.com>
2025-08-27 17:28:03 -0300

    Aula 27 de Ago.

---
+++ modulo01/Python/CURSO_PYTHON/exercicios_python_print.py
@@ -0,0 +1,59 @@
+# Questão 1
+print('João')
+# Questão 2
+print('Olá, mundo')
+# Questão 3
+print(100)
+# Questão 4
+print(7, 15, sep=" e ")
+# Questão 5
+print('NOME: João')
+print('IDADE:', 25)
+# Questão 6
+print(2+5)
+# Questão 7
+print('A soma de 5 + 8 é', 5+8)
+# Questão 8
+print('Mostrar')
+print('Palavras')
+print('Diferentes')
+# Questão 9
+print(1, 2, 3, sep=" e ")
+# Questão 10
+print("'Python é legal'")
+# Questão 11
+print('2 x 1 = ', 2*1)
+print('2 x 2 = ', 2*2)
+print('2 x 3 = ', 2*3)
+print('2 x 4 = ', 2*4)
+print('2 x 5 = ', 2*5)
+print('2 x 6 = ', 2*6)
+print('2 x 7 = ', 2*7)
+print('2 x 8 = ', 2*8)
+print('2 x 9 = ', 2*9)
+print('2 x 10 = ', 2*10)
+# Questão 12
+print('***')
+print('***')
+print('***')
+# Questão 13
+print("'Apredendo Python'")
+# Questão 14
+print('Repita'*5)
+# Questão 15
+print("'EU ESTOU APREDENTO PYTHON'")
+# Questão 16
+print(3.14)
+# Questão 17
+print(10-4)
+# Questão 18
+print('🐢')
+# Questão 19
+print('LISTA DE COMPRAS')
+print('Pão')
+print('Leite')
+print('Café')
+# Questão 20
+print('NOME: ', "João")
+print('IDADE: ', 25)
+print('CIDADE: ', "Campina Grande")
---
+++ modulo01/Python/CURSO_PYTHON/primeiro_programa.py
@@ -0,0 +1,15 @@
+print("'Hello, world!'")
+print(1+1)
+print(12, 34, sep=" = ")
+print('João') # teste
+# teste print('João')
+
+'''
+Docstring
+'''
+
+nome = "João"
+idade = 25
+
+print('Meu nome é', nome, idade)
+print('ab'+'2') 
