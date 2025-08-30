f140fad227a8974406dbcda13df5d54d27eb4f2b
JVAS42 <jvasprogramando@gmail.com>
2025-08-30 10:37:11 -0300

    Reposta lista 2

---
+++ modulo01/Python/CURSO_PYTHON/logica_inicial.py
@@ -0,0 +1,72 @@
+# Questão 1
+print("Olá Mundo")
+'''
+RESPOSTA
+LETRA → [C]
+'''
+# Questão 2
+print("Python")
+'''
+RESPOSTA
+LETRA → [C]
+'''
+# Questão 3
+print(10,20)
+'''
+RESPOSTA
+LETRA → [B]
+'''
+# Questão 4
+print("3+2")
+'''
+RESPOSTA
+LETRA → [C]
+'''
+# Questão 5
+print(3+2)
+'''
+RESPOSTA
+LETRA → [C]
+'''
+# Questão 6
+print("Python"); print("Linguagem")
+'''
+RESPOSTA
+LETRA → [C]
+'''
+# Questão 7
+print("Idade:", 20)
+'''
+RESPOSTA
+LETRA → [B]
+'''
+# Questão 8
+print('Ele disse "Oi" ')
+'''
+RESPOSTA
+LETRA → [B]
+'''
+# Questão 9
+print("Python", end="")
+'''
+RESPOSTA
+LETRA → [A]
+'''
+# Questão 10
+print("Curso", "Python")
+'''
+RESPOSTA
+LETRA → [B]
+'''
+# Questão 11
+print()
+'''
+RESPOSTA
+LETRA → [A]
+'''
+# Questão 12
+print(10,20,30)
+'''
+RESPOSTA
+LETRA → [C]
+'''
