a7b793dd691e090364afa4064d4e912c03902192
JVAS42 <jvasprogramando@gmail.com>
2025-09-16 15:22:53 -0300

    Finalizando a lista 5

--- modulo02/python/turma_06_pb/curso_python/exercicios/lista005.py
+++ modulo02/python/turma_06_pb/curso_python/exercicios/lista005.py
@@ -66,21 +66,77 @@ print(CONFERINDO)
 
 # QUESTÃO 14 #
 print('QUESTÃO 14')
+print('abc' not in 'Joao')
 
 # QUESTÃO 15 #
 print('QUESTÃO 15')
+letra = input("Digite uma letra: ")
+print(letra not in 'Joao')
 
 # QUESTÃO 16 #
 print('QUESTÃO 16')
+print('Joao'[:4])
 
 # QUESTÃO 17 #
 print('QUESTÃO 17')
+print('Joao'[-3:])
 
 # QUESTÃO 18 #
 print('QUESTÃO 18')
+print('Joao'[2:7])
 
 # QUESTÃO 19 #
 print('QUESTÃO 19')
+print('Joao'[::-1])
 
 # QUESTÃO 20 #
 print('QUESTÃO 20')
+print('Joao'[::2])
+
+# QUESTÃO 21 #
+print('QUESTÃO 21')
+print('Joao'.count('a'))
+
+# QUESTÃO 22 #
+print('QUESTÃO 22')
+letra = input("Digite uma letra: ")
+print('Joao'.find(letra))
+
+# QUESTÃO 23 #
+print('QUESTÃO 23')
+print('Joao'.startswith('Gle'))
+
+# QUESTÃO 24 #
+print('QUESTÃO 24')
+print('Joao'.endswith('ano'))
+
+# QUESTÃO 25 #
+print('QUESTÃO 25')
+for i, c in enumerate('Joao'):
+    print(i, c)
+
+# QUESTÃO 26 #
+print('QUESTÃO 26')
+palavra = input("Digite uma palavra: ")
+print(palavra in 'Joao')
+
+# QUESTÃO 27 #
+print('QUESTÃO 27')
+for c in 'Joao':
+    if c.lower() in 'aeiou':
+        print(c)
+
+# QUESTÃO 28 #
+print('QUESTÃO 28')
+for c in 'Joao':
+    if c.lower() not in 'aeiou':
+        print(c)
+
+# QUESTÃO 29 #
+print('QUESTÃO 29')
+letra = input("Digite uma letra: ")
+print('Joao'.count(letra) > 1)
+
+# QUESTÃO 30 #
+print('QUESTÃO 30')
+print('Joao' == 'Joao'[::-1])
