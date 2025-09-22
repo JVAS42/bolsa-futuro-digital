9e88227763a0b87f49a7640305bec742fd2c17db
JVAS42 <jvasprogramando@gmail.com>
2025-09-22 17:36:30 -0300

    Começando lista 6 do BFD

---
+++ modulo02/python/turma_06_pb/curso_python/exercicios/lista006.py
@@ -0,0 +1,39 @@
+# QUESTÃO 1 #
+i1 = 1
+while i1 <= 10:
+    print(i1)
+    i1 += 1
+
+# QUESTÃO 2 #
+i2 = 10
+while i2 >= 1:
+    print(i2)
+    i2 -= 1
+
+# QUESTÃO 3 #
+i3 = 0
+while i3 <= 20:
+    print(i3)
+    i3 += 2
+
+# QUESTÃO 4 #
+i4 = 1
+while i4 < 20:
+    print(i4)
+    i4 += 2
+
+# QUESTÃO 5 #
+i5 = 1
+contagem = int(input('Informe um número: '))
+while i5 < contagem:
+    print(i5)
+    i5 += 1
+
+# QUESTÃO 6 #
+palavra = str(input('Digite uma palavra: '))
+i6 = 0
+while i6 < 5:
+    print(palavra)
+    i6 += 1
+
+# QUESTÃO 7 #
--- teste.py
+++ teste.py
@@ -1,3 +1,15 @@
-frase = 'Ola, mundo!'
+vogais = ['a', 'e', 'i', 'o', 'u']
 
-print(frase)
+for i in vogais:
+    print(i)
+
+for indice, valor in enumerate(vogais):
+    print(indice, valor)
+
+for i in range(0, len(vogais), 1):
+    print(vogais[i])
+
+cont = 0
+while cont < len(vogais):
+    print(vogais[cont])
+    cont += 1
